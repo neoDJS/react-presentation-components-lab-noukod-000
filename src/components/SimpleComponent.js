2a908d169902cdda5039ee91d1fc1ec4a8a94efe
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-06-11 02:59:58 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,7 +1,11 @@
 // Code SimpleComponent Here
 import React from 'react';
 
+<<<<<<< HEAD
 export default class SimpleComponent extends React.Component {
+=======
+export default class SimpleComponent extends Reacct.Component {
+>>>>>>> ebb1b813d5d12ce087b279cf60ed669d5a663232
   state = {
     mood: "happy"
   }
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,8 +1,14 @@
 // Code SimplerComponent Here
 import React from 'react';
 
+<<<<<<< HEAD
 const SimplerComponent = (props) => {
   return (<div onClick={props.handleClick} >I am just happy</div>);
 };
 
 export default SimplerComponent
+=======
+const SimplerComponent = (props)=>{
+  return <div onClick={props.handleClick} >I am just happy</div>
+}
+>>>>>>> ebb1b813d5d12ce087b279cf60ed669d5a663232
